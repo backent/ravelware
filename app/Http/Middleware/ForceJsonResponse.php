164f05adecf58c2e56468101e2c55156dc84fe35
malik <malik@yourcheersapp.com>
2020-07-22 08:33:16 +0700

    make middleware to take request as json

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+
+class ForceJsonResponse extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
